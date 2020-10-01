01e6491c773ace8e3e06397ba746bcffe080076f
Shloka gupta <chicken-biryani@users.noreply.github.com>
2020-10-01 23:27:43 +0530

    Calculate Age of a person

---
+++ StringIsEmpty.py
@@ -0,0 +1,13 @@
+#link:https://edabit.com/challenge/EzbfiquDoAc2Zc9FL
+#problem: To check whether given string is empty or not
+
+def is_empty(s):
+	if(len(s)==0):
+		print("True")  #Could type return true
+	else:
+		print("Flase") #Could type return false
+
+s=input("Enter the string ")
+
+is_empty(s)
+	
